1afc7a661601fac854b083143b6dbf57440dd1c9
Estara <heatherjpdx@gmail.com>
2021-04-16 13:02:03 -0700

    finished

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,27 +10,29 @@ function Carousel(props) {
   const card = props.cardData[cardIdx];
   const total = props.cardData.length;
   const goForward = () => setCardIdx(cardIdx + 1);
+  const goBackward = () => setCardIdx(cardIdx - 1);
 
   return (
     <div className="Carousel">
       <h1>{props.title}</h1>
       <div className="Carousel-main">
-        <i
+        {cardIdx == 0 ? <i/> : <i
           className="fas fa-chevron-circle-left fa-2x"
-          onClick={goForward}
+          onClick={goBackward}
           data-testid="left-arrow"
-        />
+        />}
+        
         <Card
           caption={card.caption}
           src={card.src}
           currNum={cardIdx + 1}
           totalNum={total}
         />
-        <i
+        {cardIdx == (total -1) ? <i/> : <i
           className="fas fa-chevron-circle-right fa-2x"
           onClick={goForward}
           data-testid="right-arrow"
-        />
+        />}
       </div>
     </div>
   );
